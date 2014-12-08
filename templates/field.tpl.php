7b01595a7267ab91690eb57373e76a4ab5b865ba
DiegoPino <dpino@krayon.cl>
2014-12-08 16:56:07 -0300

    taxonomy field as badge

--- templates/field.tpl.php
+++ templates/field.tpl.php
@@ -56,7 +56,13 @@ HTML comment.
   <?php endif; ?>
   <div class="field-items"<?php print $content_attributes; ?>>
     <?php foreach ($items as $delta => $item):?>
-      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>><?php print render($item); ?></div>
+      <div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>
+		  <?php 
+		  $item['attributes']['classes'][]='badge';
+		  print render($item); 
+		  ?>
+	  
+	  </div>
     <?php endforeach; ?>
   </div>
 </div>
